0e514c5603540e1f705a791bd613a499b5b6aa96
Raheela <142294662+raheelam98@users.noreply.github.com>
2024-12-11 18:33:20 +0500

    Update chain.py

--- llm_ai_agents/model_2_reflection_agent/1_reflector_revisiosr_chain/chain.py
+++ llm_ai_agents/model_2_reflection_agent/1_reflector_revisiosr_chain/chain.py
@@ -1,16 +1,17 @@
 
-# creating the reflector chain and the reviosr chain
+# creating the reflector chain and the tweet reviosr chain
+# Prompt to predict, review the output, revise it, and provide suggestions for improvement
 
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-#from langchain_google_genai import ChatGoogleGenerativeAI
+# from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_openai import ChatOpenAI
 
-import os
 from dotenv import load_dotenv
 
 #Load environment variables from .env file
 load_dotenv()
 
+# Prompt to predict, review the output, revise it, and provide suggestions for improvement
 reflection_prompt = ChatPromptTemplate.from_messages(
     [
         (
@@ -22,6 +23,8 @@ reflection_prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
+# Generate a message and continuously revise it based on feedback 
+# from the reflection prompt until achieve the perfect result.
 generation_prompt = ChatPromptTemplate.from_messages(
     [
         (
@@ -30,12 +33,20 @@ generation_prompt = ChatPromptTemplate.from_messages(
            "Generate the best twitter post possible for the user's request."
            "If the user provides critique, respond with a revised version of your previous attempts."
         ),
+        # placehoder for reflection and revision
         MessagesPlaceholder(variable_name="messages")
     ]
 )
 
-#llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
-llm = ChatOpenAI()
-generation_chain = generation_prompt | llm
-reflection_chain = reflection_prompt | llm
+# Connect Prompt to LLM and Create Chain
 
+# Initialize the language model as an instance of ChatOpenAI.
+
+# llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+llm = ChatOpenAI()
+
+# Create the generation chain by combining the generation prompt with the language model.
+generation_chain = generation_prompt | llm
+
+# Create the reflection chain by combining the reflection prompt with the language model.
+reflection_chain = reflection_prompt | llm
